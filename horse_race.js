7863d8a32b3146c369c70ac1171ab7ea253c6861
Joshua Wedekind <josh@halfnibble.com>
2019-07-23 01:35:17 +0000

    Refactor horse races to use objects and distance.

--- horse_race.js
+++ horse_race.js
@@ -1,70 +1,73 @@
-// Starting horses array
-let horses = [
-    'Buttercup',        // index position 0
-    'Seabiscuit',       // index position 1
-    'Secretariat',      // index position 2
-    'My Little Pony'    // index position 3
-]
+// Starting horses object
+let horses = {
+    'Buttercup': 0,
+    'Seabiscuit': 0,
+    'Secretariat': 0,
+    'My Little Pony': 0
+}
 
 // Returns the index of the user horse selection
 let getUserHorseSelection = function(horses) {
     let horseSelectorMessage = 'What horse do you want to bet on?\n'
-    for (let i = 0; i < horses.length; i++) {
-        horseSelectorMessage += `${i + 1}. ${horses[i]}\n`
+    let horseArray = Object.keys(horses)     // ['Seabiscuit', etc..]
+    for (let i = 0; i < horseArray.length; i++) {
+        horseSelectorMessage += `${i + 1}. ${horseArray[i]}\n`
     }
     horseSelectorMessage += '\nEnter the number to select.\n'
     let userSelectedHorse = parseInt(prompt(horseSelectorMessage))
-    if (isNaN(userSelectedHorse) || userSelectedHorse < 1 || userSelectedHorse > horses.length) {
+    if (isNaN(userSelectedHorse) || userSelectedHorse < 1 || userSelectedHorse > horseArray.length) {
         alert("Invalid horse number.")
         return getUserHorseSelection(horses)
     } else {
-        return userSelectedHorse - 1
+        return horseArray[userSelectedHorse - 1]
     }
+}
+
+let sortedHorseNames = function(horses) {
+    let horseNames = Object.keys(horses).sort(function(a, b) {
+        return horses[b] - horses[a]
+    })
+    return horseNames
 }
 
 // Draw horse list in the web browser
 let renderHorsePositions = function(horses) {
     let positionEl = document.getElementById('position')
+    let horseNames = sortedHorseNames(horses)
     let html = '<ol>'
-    for (let horse of horses) {
-        html += `<li>${horse}</li>`
+    for (let horse of horseNames) {
+        html += `<li>${horse} - ${horses[horse].toFixed(2)}mi.</li>`
     }
     html += '</ol>'
     positionEl.innerHTML = html
 }
 
+let updateHorseProgress = function(horses) {
+    for (let key in horses) {
+        horses[key] += Math.random()
+    }
+}
+
+let getWinner = function(horses) {
+    return sortedHorseNames(horses)[0]
+}
+
 // Initial render
 renderHorsePositions(horses)
 
-let userSelectedHorseIndex = getUserHorseSelection(horses)
+let userSelectedHorse = getUserHorseSelection(horses)
 
 // The laps are starting here...
-
-let newHorseList = [...horses]
-
 for (let i = 0; i < 7; i++) {
     setTimeout(function() {
-        shuffle(newHorseList)
-        renderHorsePositions(newHorseList)
+        updateHorseProgress(horses)
+        renderHorsePositions(horses)
         if (i === 6) {
-            alert('The winner is: ' + newHorseList[0])
-            if (newHorseList[0] === horses[userSelectedHorseIndex]) {
+            let winner = getWinner(horses)
+            alert('The winner is: ' + winner)
+            if (winner === userSelectedHorse) {
                 alert('YOUR HORSE WON!!')
             }
         }
     }, 1000 * i)
 }
-
-
-/**
- * Shuffles array in place. ES6 version
- * @param {Array} a items An array containing the items.
- */
-function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
-}
-
