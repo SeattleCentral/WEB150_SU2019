5e478e8343f983a453e2588fb1f958c9ca1241b2
Joshua Wedekind <josh@halfnibble.com>
2019-07-23 00:09:05 +0000

    Horses game working with arrays.

--- horse_race.js
+++ horse_race.js
@@ -1,18 +1,28 @@
+// Starting horses array
 let horses = [
-    'Buttercup',
-    'Seabiscuit',
-    'Secretariat',
-    'My Little Pony'
+    'Buttercup',        // index position 0
+    'Seabiscuit',       // index position 1
+    'Secretariat',      // index position 2
+    'My Little Pony'    // index position 3
 ]
 
-// Get user horse selection
-let horseSelectorMessage = 'What horse do you want to bet on?\n'
-for (let i = 0; i < horses.length; i++) {
-    horseSelectorMessage += `${i + 1}. ${horses[i]}\n`
+// Returns the index of the user horse selection
+let getUserHorseSelection = function(horses) {
+    let horseSelectorMessage = 'What horse do you want to bet on?\n'
+    for (let i = 0; i < horses.length; i++) {
+        horseSelectorMessage += `${i + 1}. ${horses[i]}\n`
+    }
+    horseSelectorMessage += '\nEnter the number to select.\n'
+    let userSelectedHorse = parseInt(prompt(horseSelectorMessage))
+    if (isNaN(userSelectedHorse) || userSelectedHorse < 1 || userSelectedHorse > horses.length) {
+        alert("Invalid horse number.")
+        return getUserHorseSelection(horses)
+    } else {
+        return userSelectedHorse - 1
+    }
 }
-horseSelectorMessage += '\nEnter the number to select.\n'
-let userSelectedHorse = prompt(horseSelectorMessage)
 
+// Draw horse list in the web browser
 let renderHorsePositions = function(horses) {
     let positionEl = document.getElementById('position')
     let html = '<ol>'
@@ -23,7 +33,12 @@ let renderHorsePositions = function(horses) {
     positionEl.innerHTML = html
 }
 
+// Initial render
 renderHorsePositions(horses)
+
+let userSelectedHorseIndex = getUserHorseSelection(horses)
+
+// The laps are starting here...
 
 let newHorseList = [...horses]
 
@@ -33,13 +48,12 @@ for (let i = 0; i < 7; i++) {
         renderHorsePositions(newHorseList)
         if (i === 6) {
             alert('The winner is: ' + newHorseList[0])
+            if (newHorseList[0] === horses[userSelectedHorseIndex]) {
+                alert('YOUR HORSE WON!!')
+            }
         }
     }, 1000 * i)
 }
-
-
-
-
 
 
 /**
@@ -53,3 +67,4 @@ function shuffle(a) {
     }
     return a;
 }
+
