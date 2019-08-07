58b3b973073c1e50b197fe464381eea3a8061b96
Joshua Wedekind <josh@halfnibble.com>
2019-08-07 23:44:55 +0000

    Add try/catch example.

--- exceptions.js
+++ exceptions.js
@@ -15,7 +15,52 @@ function getUserNumberToDivideBy() {
     }
 }
 
-getUserNumberToDivideBy()
+// getUserNumberToDivideBy()
+
+let response = [
+    {
+        "tweet_id": 2349237492979,
+        "user": {
+            "profile_pic_url": "https://twitter.com/images/sldfjsl.png",
+            "handle": "macbookuser1",
+            "name": "Bob Marley"
+        },
+        "message": "Hello World"
+    },
+    {
+        "tweet_id": 9272374987923,
+        "message": "Buy OUR STUFF!",
+        "company": {
+            "name": "Bob's Appliances"
+        }
+    },
+    {
+        "tweet_id": 2349237492977,
+        "user": {
+            "profile_pic_url": null,
+            "handle": "otheruser2",
+            "name": "Sally Marley"
+        },
+        "message": "Howdy World"
+    }
+]
+
+let content = document.getElementById('content')
+content.innerHTML = ''
+
+for (let tweet of response) {
+    try {
+        let tweetCard = `
+            <strong>${tweet.user.name}</strong>
+            <p>${tweet.message}</strong>
+        `
+        content.innerHTML += tweetCard
+    } catch (error) {
+        console.log('An error happened rendering the following tweet:', tweet)
+        console.log('Error details: ', error)
+    }
+}
+
 
 // try {
 //     getUserNumberToDivideBy()
