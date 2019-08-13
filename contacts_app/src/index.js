e329325536c39d1bed0e4294971eb774c478fb0f
Joshua Wedekind <josh@halfnibble.com>
2019-08-13 01:51:29 +0000

    Finish regex example.

--- contacts_app/src/index.js
+++ contacts_app/src/index.js
@@ -22,6 +22,18 @@ const processForm = function(event) {
         errors.push(nameError)
     }
     
+    // Check email field
+    let emailError = getEmailFieldError(data.email)
+    if (emailError) {
+        errors.push(emailError)
+    }
+    
+    // Check phone field
+    let phoneError = getPhoneFieldError(data.phone)
+    if (phoneError) {
+        errors.push(phoneError)
+    }
+    
     // Render errors if any
     if (errors.length) {
         renderErrors(errors)
@@ -38,6 +50,24 @@ const getNameFieldError = function(nameValue) {
         return false  // no error
     } else {
         return "Invalid name value."
+    }
+}
+
+const getEmailFieldError = function(emailValue) {
+    let emailRegex = /^[a-z0-9_.\-]+@[a-z0-9\-]+(\.[a-z]+)+$/i
+    if (emailValue.match(emailRegex)) {
+        return false  // no error
+    } else {
+        return "Invalid email value."
+    }
+}
+
+const getPhoneFieldError = function(phoneValue) {
+    let phoneRegex = /^\(?[0-9]{3}-?(\) )?[0-9]{3}-?[0-9]{4}$/
+    if (phoneValue.match(phoneRegex)) {
+        return false  // no error
+    } else {
+        return "Invalid phone value."
     }
 }
 
